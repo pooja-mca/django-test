f7dad33af91b264ae802570affc33e6d5fb0adf1
pooja <kushwahapooja038@gmail.com>
2021-05-15 16:15:37 +0530

    Middleware changes updated

---
+++ app/middleware/auth.py
@@ -0,0 +1,11 @@
+from django.shortcuts import redirect
+
+def auth_middleware(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        if not request.session.get('user'):
+            return redirect(f'/')
+        response = get_response(request)
+        return response
+    return middleware
--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path
 # from.import views
 from .views import signup,index,login,logout,profile,profile_view
+from.middleware.auth import auth_middleware
 
 urlpatterns = [
     path('', index.Index.as_view(),name='index'),
     path('signup', signup.Signup.as_view(),name='signup'),
     path('login', login.Login.as_view(),name='login'),
     path('logout', logout.Logout.as_view(),name='logout'),
-    path('profile', profile.Profile.as_view(),name='profile'),
-    path('update_profile', profile.Profile.as_view(),name='profile'),
-    path('profile_view', profile_view.Profile_view.as_view(),name='profile_view'),
+    path('profile', auth_middleware(profile.Profile.as_view()),name='profile'),
+    path('update_profile', auth_middleware(profile.Profile.as_view()),name='profile'),
+    path('profile_view', auth_middleware(profile_view.Profile_view.as_view()),name='profile_view'),
 
 ]
